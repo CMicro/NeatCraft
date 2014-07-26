6f9144facd62a2c4110f1cac4ee8fb4ccd5171ae
Kiki <CMicro@users.noreply.github.com>
2014-07-26 07:01:28 +0700

    Commit main files.

---
+++ src/main/java/com/cmicro/neatcraft/NeatCraft.java
@@ -0,0 +1,10 @@
+package com.cmicro.neatcraft;
+
+import com.cmicro.neatcraft.resources.References;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = References.MOD_ID, name = References.MOD_NAME, version = References.MOD_VERSION)
+public class NeatCraft
+{
+
+}
---
+++ src/main/java/com/cmicro/neatcraft/resources/References.java
@@ -0,0 +1,8 @@
+package com.cmicro.neatcraft.resources;
+
+public class References
+{
+    public static final String MOD_ID = "NeatCraft";
+    public static final String MOD_NAME = "Neat Craft";
+    public static final String MOD_VERSION = "1.7.10-1.0";
+}
