0d114b666c91afee099241a33d94dd6d523dfdcc
CMicro <ibrahimzhilalfaruqi@gmail.com>
2014-07-26 07:23:42 +0700

    Commit assets.

--- src/main/java/com/cmicro/neatcraft/NeatCraft.java
+++ src/main/java/com/cmicro/neatcraft/NeatCraft.java
@@ -2,9 +2,31 @@ package com.cmicro.neatcraft;
 
 import com.cmicro.neatcraft.resources.References;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = References.MOD_ID, name = References.MOD_NAME, version = References.MOD_VERSION)
 public class NeatCraft
 {
+    @Mod.Instance(References.MOD_ID)
+    public static NeatCraft instance;
 
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
 }
