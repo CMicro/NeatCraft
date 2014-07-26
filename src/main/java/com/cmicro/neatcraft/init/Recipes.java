d34357797e367eaff89f7109990dc4bec3353ed7
CMicro <ibrahimzhilalfaruqi@gmail.com>
2014-07-26 12:58:09 +0700

    Updated things.

--- src/main/java/com/cmicro/neatcraft/init/Recipes.java
+++ src/main/java/com/cmicro/neatcraft/init/Recipes.java
@@ -8,7 +8,7 @@ public class Recipes
 {
     public static void init()
     {
-        //Crafting
+        //Craftings
         GameRegistry.addShapedRecipe
         (
                 new ItemStack(ModItems.itemSteel),
