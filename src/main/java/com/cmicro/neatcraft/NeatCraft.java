32d0c0a007e36e50c4387dfac41ef0e4ee468188
CMicro <ibrahimzhilalfaruqi@gmail.com>
2014-07-27 14:45:02 +0700

    Read CHANGELOG for details.

--- src/main/java/com/cmicro/neatcraft/NeatCraft.java
+++ src/main/java/com/cmicro/neatcraft/NeatCraft.java
@@ -26,7 +26,7 @@ public class NeatCraft
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        FMLCommonHandler.instance().bus().register(new ConfigHandler());
+        FMLCommonHandler.instance().bus().register(new ConfigHandler()); //c
         ConfigHandler.init(event.getSuggestedConfigurationFile());
         ModItems.init();
         ModBlocks.init();
