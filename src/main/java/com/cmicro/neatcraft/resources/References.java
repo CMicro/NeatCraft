8e6da0c3ac939db316bd4db618dc99e2112a02a5
CMicro <ibrahimzhilalfaruqi@gmail.com>
2014-08-22 20:55:03 +0700

    First signs of Emerald.

--- src/main/java/com/cmicro/neatcraft/resources/References.java
+++ src/main/java/com/cmicro/neatcraft/resources/References.java
@@ -4,7 +4,7 @@ public class References
 {
     public static final String MOD_ID = "NeatCraft";
     public static final String MOD_NAME = "Neat Craft";
-    public static final String MOD_VERSION = "1.7.10-8.8";
+    public static final String MOD_VERSION = "1.7.10-0.19.1";
     public static final String CLIENT_PROXY = "com.cmicro.neatcraft.proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.cmicro.neatcraft.proxy.ServerProxy";
     public static final String GUI_FACTORY = "com.cmicro.neatcraft.client.gui.GuiFactory";
